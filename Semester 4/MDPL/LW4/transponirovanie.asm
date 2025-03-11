b0fb960da74f8ed132bb82356115cd665403a143
Danya <danya05af@gmail.com>
2025-03-11 08:03:11 +0300

    Comlete LW4 on MDPL

--- Semester 4/MDPL/LW4/transponirovanie.asm
+++ Semester 4/MDPL/LW4/transponirovanie.asm
@@ -1,51 +1,79 @@
-; Код для подсчёта суммы элементов на главной диагонали матрицы
 text       segment
-           assume CS:text,DS:data
-begin:      mov AX, data
-            mov DS, AX
-            
-            ; Инициализация указателей
-            mov si, matrix             ; SI указывает на начало исходной матрицы
-            mov di, transposed_matrix  ; DI указывает на начало результирующей матрицы
-
-    ; Транспонирование матрицы
-    mov cx, 3                  ; Количество строк и столбцов (матрица 3x3)
-outer_loop:
-    mov bx, 0                  ; Индекс столбца
-inner_loop:
-    ; Загрузка элемента из исходной матрицы
-    mov al, [si + bx]          ; AL = matrix[row][col]
-
-    ; Сохранение элемента в транспонированную матрицу
-    mov [di + bx * 3], al      ; transposed_matrix[col][row] = AL
-
-    ; Переход к следующему столбцу
-    inc bx
-    cmp bx, 3
-    jl inner_loop              ; Повторять, пока не пройдем все столбцы
-
-    ; Переход к следующей строке
-    add si, 3                  ; Переход к следующей строке исходной матрицы
-    inc di                     ; Переход к следующей строке транспонированной матрицы
-
-    loop outer_loop               
- 
-
-end_p:
-    mov SI, 0
-    
-    mov AH,4ch
-    int 21h
-
-
+           assume CS:text, DS:data
+begin:     mov AX, data
+           mov DS, AX
+           
+           mov AX, 0
+           
+           OUTER_LOOP:
+                mov BX, 0
+                INNER_LOOP:
+                    call SWAP
+                    
+                    inc BX
+                    
+                    cmp BX, SIZEX
+                    
+                    jl INNER_LOOP
+                    
+                inc AX
+                cmp AX, SIZEY
+                
+                jl OUTER_LOOP
+                    
+           jmp END_PROGRAM
+                     
+           SWAP: 
+                push AX 
+                push BX 
+                
+                mov DX, SIZEX
+                call CALC_OFFSET
+                
+                mov SI, CX
+                
+                xchg AX, BX
+                
+                mov DX, SIZEY
+                call CALC_OFFSET 
+                
+                mov DI, CX
+                
+                mov DX, MATRIX[SI]
+                
+                mov MATRIX2[DI], DX 
+                
+                pop BX
+                pop AX
+                
+                ret
+                
+           CALC_OFFSET:
+                mov CX, AX
+                mul DX
+                add AX, BX
+                mov DX, 2
+                mul DX
+                         
+                xchg CX, AX
+                
+                ret
+           
+           END_PROGRAM:
+                mov AH,4Ch
+                mov AL,00h
+                int 21h  
+           
 text       ends
 
-data       segment
-        array   db 1, 2, 3
-                db 4, 5, 6
-                db 7, 8, 9
-        transposed_matrix db 9 dup(0)     
-data ends
+data       segment                                 
+           MATRIX    DW 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+           
+           SIZEX     DW 8
+           SIZEY     DW 2
+           MATRIX2   DW SIZEX * SIZEY dup (?)
+       
+data       ends 
 
 stk        segment stack
            db 256 dup (0)
