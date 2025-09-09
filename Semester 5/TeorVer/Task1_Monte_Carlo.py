be796907a648427e47b3fbcb1545c997b8e604f4
Danya <danya05af@gmail.com>
2025-09-09 12:09:47 +0300

    Add matplotlib graphics

--- Semester 5/TeorVer/Task1_Monte_Carlo.py
+++ Semester 5/TeorVer/Task1_Monte_Carlo.py
@@ -9,6 +9,10 @@
 # Импорт библиотек
 import tkinter as tk
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 # Создание главного окна
 root = tk.Tk()
@@ -16,4 +20,25 @@ root.title("Оценка π методом Монте-Карло")
 root.geometry("1200x800")
 root.resizable(False, False)
 
+# Matplotlib
+plt.rcParams.update({'font.size': 16})
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.set_xlim(-1.1, 1.1)
+ax.set_ylim(-1.1, 1.1)
+ax.set_aspect('equal')
 
+# Рисуем квадрат и вписанный в него круг
+ax.plot([-1, 1, 1, -1, -1], [-1, -1, 1, 1, -1], 'r-', linewidth=2)
+circle = Circle((0, 0), 1, fill=False, color='green', linewidth=2)
+ax.add_patch(circle)
+ax.set_title('Метод Монте-Карло для оценки π')
+
+# Создаём canvas для matplotlib
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.draw()
+
+# Размещаем график слева
+canvas.get_tk_widget().pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+# Запускаем главный цикл
+root.mainloop()
