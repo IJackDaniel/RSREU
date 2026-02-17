f1583e9f2f97cd3540e11dbe4ab6d28be3b73e57
Danya <danya05af@gmail.com>
2026-02-17 22:44:21 +0300

    Add function to change seed for Generator

--- Semester 6/Modelirovanie/LaboratoryWork1/src/main/java/com/IJackDaniel/UniversalGenerator.java
+++ Semester 6/Modelirovanie/LaboratoryWork1/src/main/java/com/IJackDaniel/UniversalGenerator.java
@@ -1,12 +1,12 @@
 package com.IJackDaniel;
 
 public class UniversalGenerator {
-    private static int K = 3;
+    private static final int K = 3;
     // Вспомогательные массивы
-    private static int[] _A = new int[] {171, 172, 170};
-    private static int[] _B = new int[] {177, 176, 178};
-    private static int[] _C = new int[] {2, 35, 63};
-    private static int[] _D = new int[] {30269, 30307, 30323};
+    private static final int[] _A = new int[] {171, 172, 170};
+    private static final int[] _B = new int[] {177, 176, 178};
+    private static final int[] _C = new int[] {2, 35, 63};
+    private static final int[] _D = new int[] {30269, 30307, 30323};
 
     private static int[] _Y = new int[] {1, 1, 1};
 
@@ -23,5 +23,11 @@ public class UniversalGenerator {
         }
 
         return Math.abs(s - Math.floor(s));
+    }
+
+    public static void setSeed(int[] seed) {
+        if (seed.length == K) {
+            _Y = seed.clone();
+        }
     }
 }
