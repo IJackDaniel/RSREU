4e172c70f18f1f3261a3e7931e8ff84d15548694
Danya <danya05af@gmail.com>
2026-02-17 22:37:14 +0300

    Add new class to collect statistics

--- Semester 6/Modelirovanie/LaboratoryWork1/src/main/java/com/IJackDaniel/Main.java
+++ Semester 6/Modelirovanie/LaboratoryWork1/src/main/java/com/IJackDaniel/Main.java
@@ -7,75 +7,68 @@ public class Main {
     private static final int N = 500;
     private static final int COUNT_OF_PARTS = 16;
     private static final double STEP_OF_PARTS = 1.0 / COUNT_OF_PARTS;
+    private static final StatisticsCollector STATISTICS_COLLECTOR = new StatisticsCollector();
 
     public static void main(String[] args) {
-        // Инициализация числовых характеристик
-        double sumValues = 0;
-        double sumSquaresValues = 0;
-        double sumCubedValues = 0;
-
         // Массив случайных значений
-        double[] arrOfValues = new double[N];
-        for (int i = 0; i < N; i++) {
-            arrOfValues[i] = rnd();
+        double[] arrOfValues = generateValues();
 
-            // Для характеристик
-            sumValues += arrOfValues[i];
-            sumSquaresValues += arrOfValues[i] * arrOfValues[i];
-            sumCubedValues += arrOfValues[i] * arrOfValues[i] * arrOfValues[i];
-        }
-
-        // Массив значений для гистограммы
-        int[] valuesForGist = new int[COUNT_OF_PARTS];
-        for (int i = 0; i < N; i++) {
-            int j = (int) (arrOfValues[i] / STEP_OF_PARTS);
-            valuesForGist[j] += 1;
-        }
+        // Массив значений частот для гистограммы
+        int[] frequencies = generateFrequencies(arrOfValues);
 
         // Составление названий столбцов гистограммы
-        String[] labels = new String[valuesForGist.length];
-        for (int i = 0; i < COUNT_OF_PARTS; i++) {
-            labels[i] = "[" + STEP_OF_PARTS * i + "; " + STEP_OF_PARTS * (i + 1) + "]";
-        }
+        String[] labels = generateLabels();
 
         // Вывод гистограммы
-        HistogramPrinter.printHistogram(labels, valuesForGist);
-
-        // Проверка количества значений
-        int sumCheck = 0;
-        for (int num : valuesForGist) {
-            sumCheck += num;
-        }
-        System.out.println("Проверка суммы значений: " + sumCheck);
+        HistogramPrinter.printHistogram(labels, frequencies);
 
         // Числовые характеристики
-        // Математическое ожидание
-        double expectedValue = sumValues *  (1.0 / N);
-        System.out.println("Матожидание: " + expectedValue);
-
-        // Дисперсия
-        double dispersion = sumSquaresValues * (1.0 / N);
-        dispersion = dispersion - expectedValue * expectedValue;
-        System.out.println("Дисперсия: " + dispersion);
-
-        // Второй начальный момент
-        double secondStartMoment = sumSquaresValues * (1.0 / N);
-        System.out.println("Второй начальный момент: " + secondStartMoment);
-
-        // Третий начальный момент
-        double thirdStartMoment = sumCubedValues * (1.0 / N);
-        System.out.println("Третий начальный момент: " + thirdStartMoment);
+        System.out.println("Проверка суммы значений: " + STATISTICS_COLLECTOR.getCountOfElements() +
+                        "\n" + "Матожидание: " + STATISTICS_COLLECTOR.getExpectedValue() +
+                        "\n" + "Дисперсия: " + STATISTICS_COLLECTOR.getDispersion() +
+                        "\n" + "Второй начальный момент: " + STATISTICS_COLLECTOR.getSecondStartMoment() +
+                        "\n" + "Третий начальный момент: " + STATISTICS_COLLECTOR.getThirdStartMoment()
+                );
 
         // Статистическая функция распределения
         double[] statDistribution = new double[COUNT_OF_PARTS];
         int cumulativeSum = 0;
         for (int i = 0; i < COUNT_OF_PARTS; i++) {
-            cumulativeSum += valuesForGist[i];
+            cumulativeSum += frequencies[i];
             statDistribution[i] = (double) cumulativeSum / N;
         }
         System.out.println("\nСтатистическая функция распределения:");
         for (int i = 0; i < COUNT_OF_PARTS; i++) {
             System.out.printf("%s: F(x)=%.4f\n", labels[i], statDistribution[i]);
         }
+    }
+
+    private static double[] generateValues() {
+        double[] arrOfValues = new double[N];
+        for (int i = 0; i < N; i++) {
+            double value = rnd();
+            arrOfValues[i] = value;
+
+            // Для характеристик
+            STATISTICS_COLLECTOR.addValue(value);
+        }
+        return arrOfValues;
+    }
+
+    private static int[] generateFrequencies(double[] arrayOfValues) {
+        int[] frequencies = new int[COUNT_OF_PARTS];
+        for (int i = 0; i < N; i++) {
+            int j = (int) (arrayOfValues[i] / STEP_OF_PARTS);
+            frequencies[j] += 1;
+        }
+        return frequencies;
+    }
+
+    private static String[] generateLabels() {
+        String[] labels = new String[COUNT_OF_PARTS];
+        for (int i = 0; i < COUNT_OF_PARTS; i++) {
+            labels[i] = "[" + STEP_OF_PARTS * i + "; " + STEP_OF_PARTS * (i + 1) + "]";
+        }
+        return labels;
     }
 }
