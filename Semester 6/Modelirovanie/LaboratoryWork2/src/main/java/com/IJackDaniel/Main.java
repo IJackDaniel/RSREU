2b43d440a623e98d471c5545c5f9854166b7b2ea
Danya <danya05af@gmail.com>
2026-02-28 23:12:55 +0300

    Delete excess parts

--- Semester 6/Modelirovanie/LaboratoryWork2/src/main/java/com/IJackDaniel/Main.java
+++ Semester 6/Modelirovanie/LaboratoryWork2/src/main/java/com/IJackDaniel/Main.java
@@ -1,12 +1,10 @@
 package com.IJackDaniel;
 
 public class Main {
-    // Параметры по варианту 2
-    private static final int N = 500;
-    private static final int COUNT_OF_PARTS = 16;
+    // Параметры
+    private static final int N = 20000;
+    private static final int COUNT_OF_PARTS = 20;
     private static final double STEP_OF_PARTS = 1.0 / COUNT_OF_PARTS;
-
-    private static final StatisticsCollector STATISTICS_COLLECTOR = new StatisticsCollector();
 
     public static void main(String[] args) {
         // Ввод seed для генератора
@@ -24,25 +22,11 @@ public class Main {
         // Вывод гистограммы
         HistogramPrinter.printHistogram(labels, frequencies);
 
-        // Числовые характеристики
-        System.out.println("Проверка суммы значений: " + STATISTICS_COLLECTOR.getCountOfElements() +
-                        "\n" + "Матожидание: " + STATISTICS_COLLECTOR.getExpectedValue() +
-                        "\n" + "Дисперсия: " + STATISTICS_COLLECTOR.getDispersion() +
-                        "\n" + "Второй начальный момент: " + STATISTICS_COLLECTOR.getSecondStartMoment() +
-                        "\n" + "Третий начальный момент: " + STATISTICS_COLLECTOR.getThirdStartMoment()
-                );
+        // Критерий Пирсона
 
-        // Статистическая функция распределения
-        double[] statDistribution = new double[COUNT_OF_PARTS];
-        int cumulativeSum = 0;
-        for (int i = 0; i < COUNT_OF_PARTS; i++) {
-            cumulativeSum += frequencies[i];
-            statDistribution[i] = (double) cumulativeSum / N;
-        }
-        System.out.println("\nСтатистическая функция распределения:");
-        for (int i = 0; i < COUNT_OF_PARTS; i++) {
-            System.out.printf("%s: F(x)=%.4f\n", labels[i], statDistribution[i]);
-        }
+        // Критерий Колмогорова
+
+        // Критерий числа серий (p=0.25)
     }
 
     private static double[] generateValues() {
@@ -50,9 +34,6 @@ public class Main {
         for (int i = 0; i < N; i++) {
             double value = UniversalGenerator.rnd();
             arrOfValues[i] = value;
-
-            // Для характеристик
-            STATISTICS_COLLECTOR.addValue(value);
         }
         return arrOfValues;
     }
