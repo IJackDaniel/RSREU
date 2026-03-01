89c804e7f92fb5af00e6d59e9604aeceafa283b8
Danya <danya05af@gmail.com>
2026-03-01 15:04:16 +0300

    Finish LW2

--- Semester 6/Modelirovanie/LaboratoryWork2/src/main/java/com/IJackDaniel/Main.java
+++ Semester 6/Modelirovanie/LaboratoryWork2/src/main/java/com/IJackDaniel/Main.java
@@ -3,14 +3,20 @@ package com.IJackDaniel;
 import java.util.Arrays;
 
 public class Main {
-    // Параметры
+    // Исходные параметры
     private static final int N = 20000;
     private static final int COUNT_OF_PARTS = 20;
     private static final double STEP_OF_PARTS = 1.0 / COUNT_OF_PARTS;
     private static final double P = 0.25;
 
+    // Параметры для проверки
+    private static final double ALPHA = 0.05;
+    private static final double CRITICAL_VALUE_PIRSON = 10.12;
+    private static final double BETA = 1 - ALPHA;
+    private static final double TB = 1.96;
+
+    // Параметры округления
     private static final int ACCURACY = 5;
-    private static final int ROUND = (int) Math.pow(10, ACCURACY);
 
     public static void main(String[] args) {
         // Ввод seed для генератора
@@ -19,30 +25,51 @@ public class Main {
         // Массив случайных значений
         double[] arrOfValues = generateValues();
 
-        // Массив значений частот для гистограммы
+        // Массив значений частот
         int[] frequencies = generateFrequencies(arrOfValues);
 
-        // Составление названий столбцов гистограммы
-        String[] labels = generateLabels();
+//        // Составление названий столбцов гистограммы
+//         String[] labels = generateLabels();
+//
+//        // Вывод гистограммы
+//         HistogramPrinter.printHistogram(labels, frequencies);
 
-        // Вывод гистограммы
-        HistogramPrinter.printHistogram(labels, frequencies);
+        // Вывод параметров:
+        String separator = "==============================";
+        System.out.println(separator + "Параметры" + separator +
+                "\nЧисло значений (N): " + N +
+                "\nЧисло интервалов (k): " + COUNT_OF_PARTS +
+                "\nДлина интервала: " + STEP_OF_PARTS +
+                "\nУровень значимости (альфа): " + ALPHA +
+                "\nНадёжность (бета): " + BETA +
+                "\nЗначение квантиля стандартного нормального распределения: " + TB +
+                "\n\n" + separator + "Критерии" + separator);
 
         // Критерий Пирсона
         double pirson = QualityAppraiser.computePirson(frequencies, computeTheoreticalProbabilities(), COUNT_OF_PARTS, N);
         System.out.println("Критерий Пирсона: " + round(pirson));
+        String checkPirson = (pirson <= CRITICAL_VALUE_PIRSON)? "выполнено" : "не выполнено";
+        System.out.println("Проверка равенства: " + round(pirson) + " < " + CRITICAL_VALUE_PIRSON + " - " + checkPirson);
 
         // Критерий Колмогорова
         double kolmogorov = QualityAppraiser.computeKolmogorov(arrOfValues.clone(), N);
-        System.out.println("Критерий Колмогорова: " + round(kolmogorov));
+        System.out.println("\nКритерий Колмогорова: " + round(kolmogorov));
 
         // Критерий числа серий (p=0.25)
-        int name = QualityAppraiser.getR(arrOfValues, N, P);
-        System.out.println("Критерий числа серий (p=0.25): " + name);
+        int r = QualityAppraiser.getR(arrOfValues.clone(), N, P);
+        System.out.println("\nКритерий числа серий (p=0.25): " + r);
+        double expVal = 2 * N * P * (1 - P) + P * P + (1 - P) * (1 - P);
+        double dispersion = 4 * N * P * (1 - P) * (1 - 3 * P * (1 - P)) - 2 * P * (1 - P) * (3 - 10 * P * (1 - P));
+        double sigma = Math.sqrt(dispersion);
+        double rH = expVal - TB * sigma;
+        double rB = expVal + TB * sigma;
+        String result = (rH <= r && r <= rB)? "выполнено" : "не выполнено";
+        System.out.println("Проверка равенства: " + round(rH) + " <= " + r + " <= " + round(rB) + " - " + result);
     }
 
     private static double round(double value) {
-        return (double) Math.round(value * ROUND) / ROUND;
+        int divider = (int) Math.pow(10, ACCURACY);
+        return (double) Math.round(value * divider) / divider;
     }
 
     private static double[] generateValues() {
--- Semester 6/Modelirovanie/LaboratoryWork2/src/main/java/com/IJackDaniel/QualityAppraiser.java
+++ Semester 6/Modelirovanie/LaboratoryWork2/src/main/java/com/IJackDaniel/QualityAppraiser.java
@@ -15,8 +15,9 @@ public class QualityAppraiser {
         Arrays.sort(values);
         double dMax = 0.0;
         for (int i = 0; i < n; i++) {
-            double dp = Math.abs((double) (i + 1) / n - Ft(values[i], values, n));
-            double dm = Math.abs(Ft(values[i], values, n) - (double) i / n);
+            double ft = (double) (i + 1) / n;
+            double dp = Math.abs((double) (i + 1) / n - ft);
+            double dm = Math.abs(ft - (double) i / n);
             if (dp > dMax) dMax = dp;
             if (dm > dMax) dMax = dm;
         }
@@ -28,25 +29,14 @@ public class QualityAppraiser {
         boolean y2;
         int R = 0;
 
-        if (values[0] < p) y1 = false;
-        else y1 = true;
+        y1 = !(values[0] < p);
 
         for (int i = 1; i < n; i++) {
-            if (values[i] < p) y2 = false;
-            else y2 = true;
+            y2 = !(values[i] < p);
 
             if(y1 != y2) R++;
             y1 = y2;
         }
         return R;
-    }
-
-    private static double Ft(double value, double[] values, double n) {
-        for (int i = 0; i < values.length; i++) {
-            if (values[i] == value) {
-                return (double) i / n;
-            }
-        }
-        return 0;
     }
 }
