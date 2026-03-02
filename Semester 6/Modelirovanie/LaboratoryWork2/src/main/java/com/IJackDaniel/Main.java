7ba01f984d97c5fed2a90d9b0c93f3ef10fd3df1
Danya <danya05af@gmail.com>
2026-03-02 14:09:52 +0300

    Create special class to work with quality parameters

--- Semester 6/Modelirovanie/LaboratoryWork2/src/main/java/com/IJackDaniel/Main.java
+++ Semester 6/Modelirovanie/LaboratoryWork2/src/main/java/com/IJackDaniel/Main.java
@@ -6,7 +6,6 @@ public class Main {
     // Исходные параметры
     private static final int N = 20000;
     private static final int COUNT_OF_PARTS = 20;
-    private static final double STEP_OF_PARTS = 1.0 / COUNT_OF_PARTS;
     private static final double P = 0.25;
 
     // Параметры для проверки
@@ -19,50 +18,27 @@ public class Main {
     private static final int ACCURACY = 5;
 
     public static void main(String[] args) {
-        // Ввод seed для генератора
         UniversalGenerator.setSeed(new int[]{5, 11, 3});
 
-        // Массив случайных значений
-        double[] arrOfValues = generateValues();
+        QualityAppraiser appraiser = new QualityAppraiser(generateValues(), COUNT_OF_PARTS, P, TB);
 
-        // Массив значений частот
-        int[] frequencies = generateFrequencies(arrOfValues);
-
-//        // Составление названий столбцов гистограммы
-//         String[] labels = generateLabels();
-//
-//        // Вывод гистограммы
-//         HistogramPrinter.printHistogram(labels, frequencies);
-
-        // Вывод параметров:
-        String separator = "==============================";
-        System.out.println(separator + "Параметры" + separator +
-                "\nЧисло значений (N): " + N +
-                "\nЧисло интервалов (k): " + COUNT_OF_PARTS +
-                "\nДлина интервала: " + STEP_OF_PARTS +
-                "\nУровень значимости (альфа): " + ALPHA +
-                "\nНадёжность (бета): " + BETA +
-                "\nЗначение квантиля стандартного нормального распределения: " + TB +
-                "\n\n" + separator + "Критерии" + separator);
+        appraiser.printParam();
 
         // Критерий Пирсона
-        double pirson = QualityAppraiser.computePirson(frequencies, computeTheoreticalProbabilities(), COUNT_OF_PARTS, N);
+        double pirson = appraiser.computePirson();
         System.out.println("Критерий Пирсона: " + round(pirson));
         String checkPirson = (pirson <= CRITICAL_VALUE_PIRSON)? "выполнено" : "не выполнено";
         System.out.println("Проверка равенства: " + round(pirson) + " < " + CRITICAL_VALUE_PIRSON + " - " + checkPirson);
 
         // Критерий Колмогорова
-        double kolmogorov = QualityAppraiser.computeKolmogorov(arrOfValues.clone(), N);
+        double kolmogorov = appraiser.computeKolmogorov();
         System.out.println("\nКритерий Колмогорова: " + round(kolmogorov));
 
         // Критерий числа серий (p=0.25)
-        int r = QualityAppraiser.getR(arrOfValues.clone(), N, P);
+        int r = appraiser.getR();
         System.out.println("\nКритерий числа серий (p=0.25): " + r);
-        double expVal = 2 * N * P * (1 - P) + P * P + (1 - P) * (1 - P);
-        double dispersion = 4 * N * P * (1 - P) * (1 - 3 * P * (1 - P)) - 2 * P * (1 - P) * (3 - 10 * P * (1 - P));
-        double sigma = Math.sqrt(dispersion);
-        double rH = expVal - TB * sigma;
-        double rB = expVal + TB * sigma;
+        double rH = appraiser.getrH();
+        double rB = appraiser.getrB();
         String result = (rH <= r && r <= rB)? "выполнено" : "не выполнено";
         System.out.println("Проверка равенства: " + round(rH) + " <= " + r + " <= " + round(rB) + " - " + result);
     }
@@ -79,28 +55,5 @@ public class Main {
             arrOfValues[i] = value;
         }
         return arrOfValues;
-    }
-
-    private static int[] generateFrequencies(double[] arrayOfValues) {
-        int[] frequencies = new int[COUNT_OF_PARTS];
-        for (int i = 0; i < N; i++) {
-            int j = (int) (arrayOfValues[i] / STEP_OF_PARTS);
-            frequencies[j] += 1;
-        }
-        return frequencies;
-    }
-
-    private static String[] generateLabels() {
-        String[] labels = new String[COUNT_OF_PARTS];
-        for (int i = 0; i < COUNT_OF_PARTS; i++) {
-            labels[i] = "[" + STEP_OF_PARTS * i + "; " + STEP_OF_PARTS * (i + 1) + "]";
-        }
-        return labels;
-    }
-
-    private static double[] computeTheoreticalProbabilities() {
-        double[] result = new double[COUNT_OF_PARTS];
-        Arrays.fill(result, 1.0 / COUNT_OF_PARTS);
-        return result;
     }
 }
--- Semester 6/Modelirovanie/LaboratoryWork2/src/main/java/com/IJackDaniel/QualityAppraiser.java
+++ Semester 6/Modelirovanie/LaboratoryWork2/src/main/java/com/IJackDaniel/QualityAppraiser.java
@@ -3,40 +3,116 @@ package com.IJackDaniel;
 import java.util.Arrays;
 
 public class QualityAppraiser {
-    public static double computePirson(int[] valuesFromHistogram, double[] probability, int k, int n) {
+    private int n;
+    private int countOfParts;
+    private double stepOfParts;
+
+    private double[] values;
+    private double[] sortedValues;
+    private double[] probabilities;
+    private int[] frequencies;
+
+    private double p;
+    private double tb;
+    private double expValue;
+    private double dispersion;
+    private double sigma;
+    private double rH;
+    private double rB;
+
+    public QualityAppraiser(double[] values, int parts, double p, double tb) {
+        this.n = values.length;
+        this.countOfParts = parts;
+        this.stepOfParts = 1.0 / this.countOfParts;
+
+        this.p = p;
+        this.tb = tb;
+        this.expValue = computeExpValue();
+        this.dispersion = computeDispersion();
+        this.sigma = Math.sqrt(this.dispersion);
+        this.rH = this.expValue - this.tb * this.sigma;
+        this.rB = this.expValue + this.tb * this.sigma;
+
+        this.values = values;
+        this.sortedValues = values.clone();
+        Arrays.sort(this.sortedValues);
+        this.probabilities = computeTheoreticalProbabilities();
+        this.frequencies = generateFrequencies();
+    }
+
+    public void printParam() {
+        String separator = "==============================";
+        System.out.println(separator + "Параметры" + separator +
+                "\nЧисло значений (N): " + this.n +
+                "\nЧисло интервалов (k): " + this.countOfParts +
+                "\nДлина интервала: " + this.stepOfParts +
+                "\nЗначение квантиля стандартного нормального распределения: " + this.tb + "\n\n");
+    }
+
+    public double computePirson() {
         double xi = 0.0;
-        for (int i = 0; i < k; i++) {
-            xi += Math.pow(valuesFromHistogram[i] - n * probability[i], 2) / (n * probability[i]);
+        for (int i = 0; i < this.countOfParts; i++) {
+            xi += Math.pow(this.frequencies[i] - this.n * this.probabilities[i], 2) / (this.n * this.probabilities[i]);
         }
         return xi;
     }
 
-    public static double computeKolmogorov(double[] values, int n) {
-        Arrays.sort(values);
+    public double computeKolmogorov() {
         double dMax = 0.0;
-        for (int i = 0; i < n; i++) {
-            double ft = (double) (i + 1) / n;
-            double dp = Math.abs((double) (i + 1) / n - ft);
-            double dm = Math.abs(ft - (double) i / n);
+        for (int i = 0; i < this.n; i++) {
+            double ft = (double) (i + 1) / this.n;
+            double dp = Math.abs((double) (i + 1) / this.n - ft);
+            double dm = Math.abs(ft - (double) i / this.n);
             if (dp > dMax) dMax = dp;
             if (dm > dMax) dMax = dm;
         }
-        return dMax * Math.sqrt(n);
+        return dMax * Math.sqrt(this.n);
     }
 
-    public static int getR(double[] values, int n, double p) {
+    public int getR() {
         boolean y1;
         boolean y2;
         int R = 0;
 
-        y1 = !(values[0] < p);
+        y1 = !(this.values[0] < this.p);
 
-        for (int i = 1; i < n; i++) {
-            y2 = !(values[i] < p);
+        for (int i = 1; i < this.n; i++) {
+            y2 = !(this.values[i] < this.p);
 
             if(y1 != y2) R++;
             y1 = y2;
         }
         return R;
+    }
+
+    public double getrH() {
+        return this.rH;
+    }
+
+    public double getrB() {
+        return this.rB;
+    }
+
+    private double computeExpValue() {
+        return 2 * this.n * this.p * (1 - this.p) + this.p * this.p + (1 - this.p) * (1 - this.p);
+    }
+
+    private double computeDispersion() {
+        return 4 * this.n * this.p * (1 - this.p) * (1 - 3 * this.p * (1 - this.p)) - 2 * this.p * (1 - this.p) * (3 - 10 * this.p * (1 - this.p));
+    }
+
+    private int[] generateFrequencies() {
+        int[] frequencies = new int[this.countOfParts];
+        for (int i = 0; i < this.n; i++) {
+            int j = (int) (this.values[i] / this.stepOfParts);
+            frequencies[j] += 1;
+        }
+        return frequencies;
+    }
+
+    private double[] computeTheoreticalProbabilities() {
+        double[] result = new double[this.countOfParts];
+        Arrays.fill(result, 1.0 / this.countOfParts);
+        return result;
     }
 }
