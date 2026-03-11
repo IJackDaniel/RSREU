66e3f23c5ad00c7a9b5e36997a71ecbf2ccddaef
Danya <danya05af@gmail.com>
2026-03-11 12:35:20 +0300

    Fix third task (regex)

--- Semester 6/CpSDT/02-09-Afonin/src/ru/rsreu/afonin0209/CarNumberChecker.java
+++ Semester 6/CpSDT/02-09-Afonin/src/ru/rsreu/afonin0209/CarNumberChecker.java
@@ -4,6 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CarNumberChecker {
+	private static final String CAR_NUMBER_REGEX = "^[а-яё]\\d{3}[а-яё]{2}\\d{2}rus$";
+	private static final Pattern CAR_NUMBER_PATTERN = Pattern.compile(CAR_NUMBER_REGEX);
+	
 	private CarNumberChecker() {
 		
 	}
@@ -16,15 +19,12 @@ public class CarNumberChecker {
 		return result;
 	}
 	
-	private static boolean isCarNumber(String string) {
-		String regex = "^[а-яА-ЯёЁ]\\d{3}[а-яА-ЯёЁ]{3}\\d{2}rus$";
-		
+	private static boolean isCarNumber(String string) {	
 		if (string == null || string.isEmpty()) {
 			return false;
 		}
 		
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(string);
+		Matcher matcher = CAR_NUMBER_PATTERN.matcher(string.toLowerCase());
 		return matcher.matches();
 	}
 }
--- Semester 6/CpSDT/02-09-Afonin/src/ru/rsreu/afonin0209/ClientRunner.java
+++ Semester 6/CpSDT/02-09-Afonin/src/ru/rsreu/afonin0209/ClientRunner.java
@@ -22,11 +22,11 @@ public class ClientRunner {
 		Boolean[] resultThree = CarNumberChecker.checkCarNumberInArray(strings);
 		System.out.println(RESOURCER.getString("message.output.regex") + convertArrayToString(resultThree));
 		
-		String[] resultTwo = WordsInStringProcesser.processArray(strings);
-		System.out.println(RESOURCER.getString("message.output.word") + convertArrayToString(resultTwo));
-		
-		String[] resultOne = CharacterInStringSorter.sortCharactersInStringsFromArray(strings);
-		System.out.println(RESOURCER.getString("message.output.string") + convertArrayToString(resultOne));
+//		String[] resultTwo = WordsInStringProcesser.processArray(strings);
+//		System.out.println(RESOURCER.getString("message.output.word") + convertArrayToString(resultTwo));
+//		
+//		String[] resultOne = CharacterInStringSorter.sortCharactersInStringsFromArray(strings);
+//		System.out.println(RESOURCER.getString("message.output.string") + convertArrayToString(resultOne));
 	}
 
 	private static String[] inputValues() {
