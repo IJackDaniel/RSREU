cc5d59b1daa60c6110d9c2dcd240c7dfd20a7ed4
Danya <danya05af@gmail.com>
2026-03-12 20:50:48 +0300

    Form main class

--- Semester 6/CpSDT/03-09-Afonin/src/ru/rsreu/afonin0309/ClientRunner.java
+++ Semester 6/CpSDT/03-09-Afonin/src/ru/rsreu/afonin0309/ClientRunner.java
@@ -1,9 +1,37 @@
 package ru.rsreu.afonin0309;
 
+import java.util.Arrays;
+import java.util.Locale;
+
+import com.prutzkow.resourcer.ProjectResourcer;
+import com.prutzkow.resourcer.Resourcer;
+
 public class ClientRunner {
-
-	public static void main(String[] args) {
-
+	private static final Resourcer RESOURCER = ProjectResourcer.getInstance();
+	private static final int[] NUMBERS = new int[] { 1, 2, 3, 4, 5};
+	
+	private ClientRunner() {
+		
 	}
-
+	
+	public static void main(String[] args) {
+		StringBuilder stringBuilder = new StringBuilder();
+		B b = new B(new A(ClientRunner.NUMBERS));
+		b.method();
+		
+		ClientRunner.appendResultToBuilder(stringBuilder, b);
+		
+		Locale.setDefault(Locale.US);
+		
+		ClientRunner.appendResultToBuilder(stringBuilder, b);
+		
+		System.out.println(stringBuilder.toString());
+	}
+	
+	private static void appendResultToBuilder(StringBuilder stringBuilder, B b) {
+		stringBuilder.append(RESOURCER.getString("message.output.originalArray"));
+		stringBuilder.append(Arrays.toString(ClientRunner.NUMBERS));
+		stringBuilder.append(RESOURCER.getString("message.output.processedArray"));
+		stringBuilder.append(b.getData());
+	}
 }
