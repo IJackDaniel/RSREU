c9798a68730de4f09ab5a53daca8c64a71113309
Danya <danya05af@gmail.com>
2026-03-16 23:35:44 +0300

    Add programm structure with method stubs

--- Semester 6/CpSDT/04-09-Afonin/src/ru/rsreu/afonin0409/A.java
+++ Semester 6/CpSDT/04-09-Afonin/src/ru/rsreu/afonin0409/A.java
@@ -1,5 +1,27 @@
 package ru.rsreu.afonin0409;
 
-public class A {
+import com.prutzkow.twodimarray.TwoDimArray;
 
+// Преобразовать элементы строки массива так, 
+// чтобы сначала располагались элементы с ненулевыми значениями
+// без изменения их порядке, а затем - с нулевыми
+public class A extends TwoDimArray {
+	
+	public A(int... sizes) throws IllegalArgumentException{
+		super(sizes);
+	}
+	
+	public void method() {
+		
+	}
+	
+	public void fill(int lowerBound, int upperBound) {
+		
+	}
+	
+	// Add count of elements for first column
+	@Override
+	public String toString() {
+		return "";
+	}
 }
--- Semester 6/CpSDT/04-09-Afonin/src/ru/rsreu/afonin0409/ClientRunner.java
+++ Semester 6/CpSDT/04-09-Afonin/src/ru/rsreu/afonin0409/ClientRunner.java
@@ -5,9 +5,19 @@ import com.prutzkow.resourcer.Resourcer;
 
 public class ClientRunner {
 	private static final Resourcer RESOURCER = ProjectResourcer.getInstance();
+	private static final int[] SIZES = new int[] {5, 4, 4, 3, 6, 5};
+	private static final int LOWER_BOUND = -5;
+	private static final int UPPER_BOUND = 5;
 	
 	public static void main(String[] args) {
+		A a = new A(ClientRunner.SIZES);
+		a.fill(ClientRunner.LOWER_BOUND, ClientRunner.UPPER_BOUND);
 		
+		// Add original two dimension array to stringBuilder
+		
+		a.method();
+		
+		// Add processed two dimension array to stringBuilder
 
 	}
 
