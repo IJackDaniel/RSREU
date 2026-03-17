53daa02e19f984c7323420eec5265eeedb939ee7
Danya <danya05af@gmail.com>
2026-03-17 21:46:37 +0300

    Delete excess methods and fields from DriverLicense class

--- Semester 6/CpSDT/UnnaturalCode/src/com/prutzkow/unnaturalcode/p01caranddriverlicense/DriverLicense.java
+++ Semester 6/CpSDT/UnnaturalCode/src/com/prutzkow/unnaturalcode/p01caranddriverlicense/DriverLicense.java
@@ -1,22 +1,8 @@
 package com.prutzkow.unnaturalcode.p01caranddriverlicense;
 
 class DriverLicense {
-	private boolean isTeenDriver;
-	private boolean isAdult;
-	private boolean isLearner;
-	public DriverLicense(boolean isTeenDriver, boolean isAdult,
-			boolean isLearner) {
-		this.isTeenDriver = isTeenDriver;
-		this.isAdult = isAdult;
-		this.isLearner = isLearner;
-	}
-	public boolean isTeenDriver() {
-		return isTeenDriver;
-	}
-	public boolean isAdult() {
-		return isAdult;
-	}
-	public boolean isLearner() {
-		return isLearner;
+
+	public DriverLicense() {
+		
 	}
 }
