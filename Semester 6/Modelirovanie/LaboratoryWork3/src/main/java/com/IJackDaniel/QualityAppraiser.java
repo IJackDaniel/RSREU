0e576c001ff6e9b2b31e668afe01bb2c41663b33
Danya <danya05af@gmail.com>
2026-03-25 12:13:22 +0300

    Move files from LW2

---
+++ Semester 6/Modelirovanie/LaboratoryWork3/src/main/java/com/IJackDaniel/QualityAppraiser.java
@@ -0,0 +1,204 @@
+package com.IJackDaniel;
+
+import java.util.Arrays;
+
+public class QualityAppraiser {
+    // Параметры последовательности
+    private final int length;
+    private final int countOfParts;
+    private final double stepOfParts;
+
+    // Последовательности данных
+    private final double[] values;
+    private double[] sortedValues;
+    private final double[] probabilities;
+    private final int[] frequencies;
+
+    // Общие параметры
+    private final double alpha;
+    private final double beta;
+
+    // Параметры для критерия Пирсона
+    private final int r;
+    private final double criticalValuePirson;
+
+    // Параметры для критерия числа серий
+    private final double p;
+    private final double tb;
+    private double rH;
+    private double rB;
+
+    // Значения критериев
+    private double pirson;
+    private double kolmogorov;
+    private int R;
+
+    // Параметры для вывода
+    private final String SEPARATOR_1 = "=";
+    private final String SEPARATOR_2 = "_";
+    private final int LENGTH_OF_INPUT = 65;
+    private static final int ACCURACY = 5;
+
+    public QualityAppraiser(double[] values, int parts, double alpha, double p) {
+        this.length = values.length;
+        this.countOfParts = parts;
+        this.stepOfParts = this.round(1.0 / this.countOfParts);
+        this.alpha = alpha;
+        this.beta = this.round(1 - this.alpha);
+
+        this.values = values;
+        this.sortedValues = values.clone();
+        Arrays.sort(this.sortedValues);
+        this.probabilities = computeTheoreticalProbabilities();
+        this.frequencies = generateFrequencies();
+        this.r = this.countOfParts - 1;
+        this.criticalValuePirson = PirsonCriticalValues.getPirsonCriticalValue(this.r, this.beta);
+
+        this.p = p;
+        this.tb = NormalQuantiles.getTB(this.alpha);
+        computeBoundsForR();
+
+        computePirson();
+        computeKolmogorov();
+        computeR();
+    }
+
+    public void printParam() {
+        System.out.println(createSeparatorLine("Параметры", this.SEPARATOR_1, this.LENGTH_OF_INPUT) +
+                "\n" + createSeparatorLine("Общие", this.SEPARATOR_2, this.LENGTH_OF_INPUT) +
+                "\nЧисло значений (N): " + this.length +
+                "\nЧисло интервалов (k): " + this.countOfParts +
+                "\nДлина интервала: " + this.stepOfParts +
+                "\nУровень значимости (alpha): " + this.alpha +
+                "\nНадёжность (beta): " + this.beta +
+                "\n\n" + createSeparatorLine("Для критерия Пирсона", this.SEPARATOR_2, this.LENGTH_OF_INPUT) +
+                "\nЧисло степеней свободы (r): " + this.r +
+                "\nКритическое значение: " + this.criticalValuePirson +
+                "\n\n" + createSeparatorLine("Для критерия числа серий", this.SEPARATOR_2, this.LENGTH_OF_INPUT) +
+                "\nРазделительный элемент (p): " + this.p +
+                "\nЗначение квантиля стандартного нормального распределения: " + this.tb +
+                "\nНижняя граница (RH): " + this.rH +
+                "\nВерхняя граница (RB): " + this.rB +
+                "\n" + createSeparatorLine("", this.SEPARATOR_1, this.LENGTH_OF_INPUT) + "\n");
+    }
+
+    public void computePirson() {
+        double xi = 0.0;
+        for (int i = 0; i < this.countOfParts; i++) {
+            xi += Math.pow(this.frequencies[i] - this.length * this.probabilities[i], 2) /
+                    (this.length * this.probabilities[i]);
+        }
+        this.pirson = this.round(xi);
+    }
+
+    public void computeKolmogorov() {
+        double dMax = 0.0;
+        for (int i = 0; i < this.length; i++) {
+            double ft = (double) (i + 1) / this.length;
+            double dp = Math.abs((double) (i + 1) / this.length - ft);
+            double dm = Math.abs(ft - (double) i / this.length);
+            if (dp > dMax) dMax = dp;
+            if (dm > dMax) dMax = dm;
+        }
+        this.kolmogorov = this.round(dMax * Math.sqrt(this.length));
+    }
+
+    public void computeR() {
+        boolean y1;
+        boolean y2;
+        int r = 0;
+
+        y1 = !(this.values[0] < this.p);
+
+        for (int i = 1; i < this.length; i++) {
+            y2 = !(this.values[i] < this.p);
+
+            if(y1 != y2) r++;
+            y1 = y2;
+        }
+        this.R = r;
+    }
+
+    public double getrH() {
+        return this.rH;
+    }
+
+    public double getrB() {
+        return this.rB;
+    }
+
+    public double getPirsonCriticalValue() {
+        return this.criticalValuePirson;
+    }
+
+    public boolean checkBoundsForR() {
+        return (this.rH <= this.R && this.R <= this.rB);
+    }
+
+    public double getPirson() {
+        return pirson;
+    }
+
+    public double getKolmogorov() {
+        return kolmogorov;
+    }
+
+    public int getR() {
+        return R;
+    }
+
+    public int[] getFrequencies() {
+        return this.frequencies.clone();
+    }
+
+    private void computeBoundsForR() {
+        double expValue = computeExpValue();
+        double dispersion = computeDispersion();
+        double sigma = Math.sqrt(dispersion);
+        this.rH = this.round(expValue - this.tb * sigma);
+        this.rB = this.round(expValue + this.tb * sigma);
+    }
+
+    private double computeExpValue() {
+        return 2 * this.length * this.p * (1 - this.p) + this.p * this.p + (1 - this.p) * (1 - this.p);
+    }
+
+    private double computeDispersion() {
+        return 4 * this.length * this.p * (1 - this.p) * (1 - 3 * this.p * (1 - this.p)) -
+                2 * this.p * (1 - this.p) * (3 - 10 * this.p * (1 - this.p));
+    }
+
+    private int[] generateFrequencies() {
+        int[] frequencies = new int[this.countOfParts];
+        for (int i = 0; i < this.length; i++) {
+            int j = (int) (this.values[i] / this.stepOfParts);
+            frequencies[j] += 1;
+        }
+        return frequencies;
+    }
+
+    private double[] computeTheoreticalProbabilities() {
+        double[] result = new double[this.countOfParts];
+        Arrays.fill(result, 1.0 / this.countOfParts);
+        return result;
+    }
+
+    private String createSeparatorLine(String word, String separatorChar, int totalLength) {
+        String processedWord = word;
+
+        if (processedWord.length() >= totalLength) {
+            return processedWord;
+        }
+
+        int remainingLength = totalLength - processedWord.length();
+        int leftPadding = remainingLength / 2;
+        int rightPadding = remainingLength - leftPadding;
+
+        return separatorChar.repeat(leftPadding) + processedWord + separatorChar.repeat(rightPadding);
+    }
+
+    private double round(double value) {
+        int divider = (int) Math.pow(10, ACCURACY);
+        return (double) Math.round(value * divider) / divider;
+    }
+}
---
+++ Semester 6/Modelirovanie/LaboratoryWork3/src/main/java/com/IJackDaniel/UniversalGenerator.java
@@ -0,0 +1,42 @@
+package com.IJackDaniel;
+
+public class UniversalGenerator {
+    private static final int K = 3;
+    // Вспомогательные массивы
+    private static final int[] _A = new int[] {171, 172, 170};
+    private static final int[] _B = new int[] {177, 176, 178};
+    private static final int[] _C = new int[] {2, 35, 63};
+    private static final int[] _D = new int[] {30269, 30307, 30323};
+
+    private static int[] _Y = new int[] {1, 1, 1};
+
+    public static void setSeed(int[] seed) {
+        if (seed.length == K) {
+            _Y = seed.clone();
+        }
+    }
+
+    public static double[] generateValues(int n) {
+        double[] arrOfValues = new double[n];
+        for (int i = 0; i < n; i++) {
+            double value = UniversalGenerator.rnd();
+            arrOfValues[i] = value;
+        }
+        return arrOfValues;
+    }
+
+    private static double rnd() {
+        double s = 0.0;
+        double[] x = new double[K];
+        int[] yN = new int[K];
+
+        for (int i = 0; i < K; i++) {
+            yN[i] = (int) Math.abs(_A[i] * (_Y[i] % _B[i]) - _C[i] * (double) _Y[i] / _B[i]);
+            _Y[i] = yN[i];
+            x[i] = (double) yN[i] / _D[i];
+            s += x[i];
+        }
+
+        return Math.abs(s - Math.floor(s));
+    }
+}
