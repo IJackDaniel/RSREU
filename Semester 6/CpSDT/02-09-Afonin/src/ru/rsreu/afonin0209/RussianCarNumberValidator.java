fa8aa2cae6f1160f777b8a438a14fb182019f085
Danya <danya05af@gmail.com>
2026-03-27 16:57:04 +0300

    Fix validator for car numbers

--- Semester 6/CpSDT/02-09-Afonin/src/ru/rsreu/afonin0209/RussianCarNumberValidator.java
+++ Semester 6/CpSDT/02-09-Afonin/src/ru/rsreu/afonin0209/RussianCarNumberValidator.java
@@ -4,7 +4,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RussianCarNumberValidator {
-	private static final String CAR_NUMBER_REGEX = "^[а-яё]\\d{3}[а-яё]{2}\\d{2}rus$";
+	private static final String VALID_LETTERS = "АВЕКМНОРСТУХ";
+	private static final String CAR_NUMBER_REGEX = "^[" + VALID_LETTERS + "]\\d{3}[" + VALID_LETTERS + "]{2}" + "(?:"
+			// 01-99
+			+ "0[1-9]|[1-9][0-9]|"
+			// 101-199
+			+ "101|102|103|105|109|111|113|115|116|118|121|122|123|125|126|127|130|131|134|136|"
+			+ "138|139|142|147|150|152|154|155|156|158|159|161|163|164|169|172|173|174|177|178|"
+			+ "180|181|182|184|185|186|188|190|192|193|196|197|198|199|"
+			// additional codes
+			+ "250|252|323|550|702|716|725|750|754|761|763|774|777|790|797|799|977" + ")rus$";
 	private static final Pattern CAR_NUMBER_PATTERN = Pattern.compile(RussianCarNumberValidator.CAR_NUMBER_REGEX);
 
 	private RussianCarNumberValidator() {
@@ -24,7 +33,7 @@ public class RussianCarNumberValidator {
 			return false;
 		}
 
-		Matcher matcher = RussianCarNumberValidator.CAR_NUMBER_PATTERN.matcher(string.toLowerCase());
+		Matcher matcher = RussianCarNumberValidator.CAR_NUMBER_PATTERN.matcher(string);
 		return matcher.matches();
 	}
 }
