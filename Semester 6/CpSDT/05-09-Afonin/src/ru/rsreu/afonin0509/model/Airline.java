2e64ff9d5cb7f4bc68f1eda97b9d9670a0adfd2e
Danya <danya05af@gmail.com>
2026-03-31 00:02:43 +0300

    Implement Airline class

--- Semester 6/CpSDT/05-09-Afonin/src/ru/rsreu/afonin0509/model/Airline.java
+++ Semester 6/CpSDT/05-09-Afonin/src/ru/rsreu/afonin0509/model/Airline.java
@@ -1,5 +1,49 @@
 package ru.rsreu.afonin0509.model;
 
-public class Airline {
+import java.util.Arrays;
 
+import ru.rsreu.afonin0509.model.aircraft.Aircraft;
+import ru.rsreu.afonin0509.model.aircraft.NullAircraft;
+
+public class Airline {
+	
+	private final Aircraft[] fleet;
+	
+	public Airline(Aircraft[] fleet) {
+		this.fleet = fleet;
+	}
+	
+	public int getTotalPassengersCapacity() {
+		return Arrays.stream(this.fleet)
+				.mapToInt(Aircraft::getPassengerCapacity)
+				.sum();
+	}
+	
+	public double getTotalCargoCapacity() {
+		return Arrays.stream(this.fleet)
+				.mapToDouble(Aircraft::getCargoCapacity)
+				.sum();
+	}
+	
+	public boolean canTransport(int passengers, double cargo) {
+		int totalPassengerCapacity = this.getTotalPassengersCapacity();
+		double totalCargoCapacity = this.getTotalCargoCapacity();
+		return totalPassengerCapacity >= passengers && totalCargoCapacity >= cargo;
+	}
+	
+	public void sortByFlightRange() {
+		Arrays.sort(this.fleet);
+	}
+	
+	public Aircraft findByModel(String model) {
+		Aircraft result = Arrays.stream(this.fleet)
+				.filter(a -> a.getModel().equals(model))
+				.findFirst()
+				.orElse(null);
+		
+		if (result == null) {
+			return new NullAircraft();
+		}
+		return result;
+	}
 }
