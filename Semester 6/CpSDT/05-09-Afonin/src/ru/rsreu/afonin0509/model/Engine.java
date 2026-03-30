92c0e0a38664f115d115877de21a1bfb6455772e
Danya <danya05af@gmail.com>
2026-03-30 21:43:59 +0300

    Finish Engine class

--- Semester 6/CpSDT/05-09-Afonin/src/ru/rsreu/afonin0509/model/Engine.java
+++ Semester 6/CpSDT/05-09-Afonin/src/ru/rsreu/afonin0509/model/Engine.java
@@ -1,5 +1,20 @@
 package ru.rsreu.afonin0509.model;
 
 public class Engine {
-
+	
+	private final String type;
+	private final int power;
+	
+	Engine(String type, int power) {
+		this.type = type;
+		this.power = power;
+	}
+	
+	String getType() {
+		return this.type;
+	}
+	
+	int getPower() {
+		return power;
+	}
 }
