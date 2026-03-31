e9f13a7d0ccc893256cb4209bf5309fa70423664
Danya <danya05af@gmail.com>
2026-03-31 16:15:01 +0300

    Delete magic numbers. Change to enum with indexes

--- Semester 6/CpSDT/05-09-Afonin/src/ru/rsreu/afonin0509/view/AircraftToTableRowConverter.java
+++ Semester 6/CpSDT/05-09-Afonin/src/ru/rsreu/afonin0509/view/AircraftToTableRowConverter.java
@@ -3,7 +3,24 @@ package ru.rsreu.afonin0509.view;
 import ru.rsreu.afonin0509.model.aircraft.Aircraft;
 
 public class AircraftToTableRowConverter {
-
+	
+	private enum ColumnIndex {
+		TYPE(0),
+		MODEL(1),
+		MANUFACTURER(2),
+		RANGE(3);
+		
+		private final int index;
+		
+		ColumnIndex(int index) {
+			this.index = index;
+		}
+		
+		public int getIndex() {
+			return this.index;
+		}
+	}
+	
 	private AircraftToTableRowConverter() {
 
 	}
@@ -12,10 +29,10 @@ public class AircraftToTableRowConverter {
 		String[][] data = new String[fleet.length][4];
 
 		for (int i = 0; i < fleet.length; i++) {
-			data[i][0] = fleet[i].getClass().getSimpleName();
-			data[i][1] = fleet[i].getModel();
-			data[i][2] = fleet[i].getManufacturer().name();
-			data[i][3] = String.valueOf(fleet[i].getFlightRange());
+			data[i][ColumnIndex.TYPE.getIndex()] = fleet[i].getClass().getSimpleName();
+			data[i][ColumnIndex.MODEL.getIndex()] = fleet[i].getModel();
+			data[i][ColumnIndex.MANUFACTURER.getIndex()] = fleet[i].getManufacturer().name();
+			data[i][ColumnIndex.RANGE.getIndex()] = String.valueOf(fleet[i].getFlightRange());
 		}
 
 		return data;
