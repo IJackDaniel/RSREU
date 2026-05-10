1b3a7c75f6b69a72ffdd198cfb26f8f27812a2f5
Danya <danya05af@gmail.com>
2026-05-10 18:42:40 +0300

    Fix exceptions

--- Semester 6/CpSDT/06-17-Afonin/src/ru/rsreu/afonin0617/service/EnterpriseFileService.java
+++ Semester 6/CpSDT/06-17-Afonin/src/ru/rsreu/afonin0617/service/EnterpriseFileService.java
@@ -1,9 +1,17 @@
 package ru.rsreu.afonin0617.service;
 
-import java.io.*;
-import java.nio.file.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 import com.prutzkow.projectlogger.ProjectLogger;
@@ -16,6 +24,8 @@ import ru.rsreu.afonin0617.model.OwnershipForm;
 public class EnterpriseFileService {
 
 	private static final Resourcer RESOURCER = ProjectResourcer.getInstance();
+
+	private static final int ENTERPRISE_DATA_PARTS_COUNT = 3;
 
 	private static final String SOURCE_DIRECTORY_NAME = RESOURCER.getString("files.folder.source.name");
 
@@ -30,14 +40,16 @@ public class EnterpriseFileService {
 	public String createSourceFileWithEnterpriseData(Enterprise[] enterprises) {
 
 		try {
+
 			Path sourceDirectoryPath = Paths.get(EnterpriseFileService.SOURCE_DIRECTORY_NAME);
+
 			Path copyDirectoryPath = sourceDirectoryPath.resolve(EnterpriseFileService.COPY_SUBDIRECTORY_NAME);
+
+			Path sourceFilePath = sourceDirectoryPath.resolve(EnterpriseFileService.DATA_FILE_NAME);
 
 			Files.createDirectories(copyDirectoryPath);
 
 			ProjectLogger.logger.log(Level.INFO, "Created directory: " + copyDirectoryPath.toAbsolutePath());
-
-			Path sourceFilePath = sourceDirectoryPath.resolve(EnterpriseFileService.DATA_FILE_NAME);
 
 			this.writeEnterpriseArrayToFile(sourceFilePath, enterprises);
 
@@ -45,11 +57,33 @@ public class EnterpriseFileService {
 
 			return sourceFilePath.toString();
 
+		} catch (InvalidPathException exception) {
+
+			String message = RESOURCER.getString("error.directory.create.invalid_path")
+					+ EnterpriseFileService.SOURCE_DIRECTORY_NAME + ". "
+					+ RESOURCER.getString("error.directory.create.invalid_path.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
+		} catch (FileSystemException exception) {
+
+			String message = RESOURCER.getString("error.directory.create.locked")
+					+ EnterpriseFileService.SOURCE_DIRECTORY_NAME + ". "
+					+ RESOURCER.getString("error.directory.create.locked.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
 		} catch (IOException exception) {
 
-			String message = RESOURCER.getString("error.file.write") + EnterpriseFileService.SOURCE_DIRECTORY_NAME;
+			String message = RESOURCER.getString("error.directory.create.io")
+					+ EnterpriseFileService.SOURCE_DIRECTORY_NAME + ". "
+					+ RESOURCER.getString("error.directory.create.io.fix");
 
-			ProjectLogger.logger.log(Level.SEVERE, message, exception);
+			ProjectLogger.logger.log(Level.SEVERE, message);
 
 			throw new RuntimeException(message);
 		}
@@ -58,6 +92,7 @@ public class EnterpriseFileService {
 	public String copySourceFileToBackup(String sourceFilePath) {
 
 		try {
+
 			Path sourcePath = Paths.get(sourceFilePath);
 
 			String backupFileName = this.buildBackupFileName(sourcePath);
@@ -65,15 +100,43 @@ public class EnterpriseFileService {
 			Path backupFilePath = sourcePath.getParent().resolve(EnterpriseFileService.COPY_SUBDIRECTORY_NAME)
 					.resolve(backupFileName);
 
-			this.copyFile(sourcePath, backupFilePath);
+			this.copyFileToBackupLocation(sourcePath, backupFilePath);
 
 			return backupFilePath.toString();
 
+		} catch (NoSuchFileException exception) {
+
+			String message = RESOURCER.getString("error.copy.no_such_file") + sourceFilePath + ". "
+					+ RESOURCER.getString("error.copy.no_such_file.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
+		} catch (InvalidPathException exception) {
+
+			String message = RESOURCER.getString("error.copy.invalid_path") + sourceFilePath + ". "
+					+ RESOURCER.getString("error.copy.invalid_path.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
+		} catch (FileSystemException exception) {
+
+			String message = RESOURCER.getString("error.copy.locked") + sourceFilePath + ". "
+					+ RESOURCER.getString("error.copy.locked.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
 		} catch (IOException exception) {
 
-			String message = RESOURCER.getString("error.file.copy") + sourceFilePath;
+			String message = RESOURCER.getString("error.copy.io") + sourceFilePath + ". "
+					+ RESOURCER.getString("error.copy.io.fix");
 
-			ProjectLogger.logger.log(Level.SEVERE, message, exception);
+			ProjectLogger.logger.log(Level.SEVERE, message);
 
 			throw new RuntimeException(message);
 		}
@@ -82,10 +145,14 @@ public class EnterpriseFileService {
 	public String moveSourceFileToTargetDirectory(String sourceFilePath) {
 
 		try {
+
 			Path sourcePath = Paths.get(sourceFilePath);
+
 			Path targetDirectory = Paths.get(EnterpriseFileService.MOVE_DIRECTORY_NAME);
 
 			Files.createDirectories(targetDirectory);
+
+			ProjectLogger.logger.log(Level.INFO, "Created directory: " + targetDirectory.toAbsolutePath());
 
 			Path targetFilePath = targetDirectory.resolve(sourcePath.getFileName());
 
@@ -95,11 +162,39 @@ public class EnterpriseFileService {
 
 			return targetFilePath.toString();
 
+		} catch (NoSuchFileException exception) {
+
+			String message = RESOURCER.getString("error.move.no_such_file") + sourceFilePath + ". "
+					+ RESOURCER.getString("error.move.no_such_file.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
+		} catch (InvalidPathException exception) {
+
+			String message = RESOURCER.getString("error.move.invalid_path") + sourceFilePath + ". "
+					+ RESOURCER.getString("error.move.invalid_path.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
+		} catch (FileSystemException exception) {
+
+			String message = RESOURCER.getString("error.move.locked") + sourceFilePath + ". "
+					+ RESOURCER.getString("error.move.locked.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
 		} catch (IOException exception) {
 
-			String message = RESOURCER.getString("error.file.move") + sourceFilePath;
+			String message = RESOURCER.getString("error.move.io") + sourceFilePath + ". "
+					+ RESOURCER.getString("error.move.io.fix");
 
-			ProjectLogger.logger.log(Level.SEVERE, message, exception);
+			ProjectLogger.logger.log(Level.SEVERE, message);
 
 			throw new RuntimeException(message);
 		}
@@ -108,17 +203,55 @@ public class EnterpriseFileService {
 	public Enterprise[] readEnterprisesFromFile(String filePath) {
 
 		try {
+
 			List<Enterprise> enterpriseList = new ArrayList<>();
 
 			this.readEnterpriseFileLines(filePath, enterpriseList);
 
 			return enterpriseList.toArray(new Enterprise[0]);
 
+		} catch (NoSuchFileException exception) {
+
+			String message = RESOURCER.getString("error.read.no_such_file") + filePath + ". "
+					+ RESOURCER.getString("error.read.no_such_file.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
+		} catch (InvalidPathException exception) {
+
+			String message = RESOURCER.getString("error.read.invalid_path") + filePath + ". "
+					+ RESOURCER.getString("error.read.invalid_path.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
+		} catch (FileSystemException exception) {
+
+			String message = RESOURCER.getString("error.read.locked") + filePath + ". "
+					+ RESOURCER.getString("error.read.locked.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
+		} catch (IllegalArgumentException exception) {
+
+			String message = RESOURCER.getString("error.read.invalid_data") + filePath + ". "
+					+ RESOURCER.getString("error.read.invalid_data.fix");
+
+			ProjectLogger.logger.log(Level.SEVERE, message);
+
+			throw new RuntimeException(message);
+
 		} catch (IOException exception) {
 
-			String message = RESOURCER.getString("error.file.read") + filePath;
+			String message = RESOURCER.getString("error.read.io") + filePath + ". "
+					+ RESOURCER.getString("error.read.io.fix");
 
-			ProjectLogger.logger.log(Level.SEVERE, message, exception);
+			ProjectLogger.logger.log(Level.SEVERE, message);
 
 			throw new RuntimeException(message);
 		}
@@ -129,27 +262,30 @@ public class EnterpriseFileService {
 		try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
 
 			for (Enterprise enterprise : enterprises) {
+
 				writer.write(enterprise.toString());
+
 				writer.newLine();
 			}
 		}
 	}
 
-	private void copyFile(Path source, Path target) throws IOException {
+	private void copyFileToBackupLocation(Path source, Path target) throws IOException {
 
 		Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
 
 		ProjectLogger.logger.log(Level.INFO, "Copied file: " + target.toAbsolutePath());
 	}
 
-	private void readEnterpriseFileLines(String filePath, List<Enterprise> list) throws IOException {
+	private void readEnterpriseFileLines(String filePath, List<Enterprise> enterpriseList) throws IOException {
 
 		try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
 
 			String line;
 
 			while ((line = reader.readLine()) != null) {
-				list.add(this.parseEnterpriseLine(line));
+
+				enterpriseList.add(this.parseEnterpriseLine(line));
 			}
 		}
 	}
@@ -158,24 +294,31 @@ public class EnterpriseFileService {
 
 		String[] parts = line.split(";");
 
+		if (parts.length != EnterpriseFileService.ENTERPRISE_DATA_PARTS_COUNT) {
+
+			throw new IllegalArgumentException(RESOURCER.getString("error.read.invalid_data.internal"));
+		}
+
 		return new Enterprise(parts[0], parts[1], OwnershipForm.valueOf(parts[2]));
 	}
 
 	private String buildBackupFileName(Path sourceFilePath) {
 
-	    String originalFileName = sourceFilePath.getFileName().toString();
+		String originalFileName = sourceFilePath.getFileName().toString();
 
-	    int extensionSeparatorIndex = originalFileName.lastIndexOf('.');
+		int extensionSeparatorIndex = originalFileName.lastIndexOf('.');
 
-	    String fileNameWithoutExtension;
+		String fileNameWithoutExtension;
 
-	    if (extensionSeparatorIndex > 0) {
-	        fileNameWithoutExtension =
-	                originalFileName.substring(0, extensionSeparatorIndex);
-	    } else {
-	        fileNameWithoutExtension = originalFileName;
-	    }
+		if (extensionSeparatorIndex > 0) {
 
-	    return fileNameWithoutExtension + "." + EnterpriseFileService.BACKUP_FILE_EXTENSION;
+			fileNameWithoutExtension = originalFileName.substring(0, extensionSeparatorIndex);
+
+		} else {
+
+			fileNameWithoutExtension = originalFileName;
+		}
+
+		return fileNameWithoutExtension + "." + EnterpriseFileService.BACKUP_FILE_EXTENSION;
 	}
 }
